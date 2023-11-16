87ce8dd3b2466a9d9922ca4b349e9185bcf778b0
Evgeny <florans1807@mail.ru>
2023-11-16 16:28:51 +0300

    logout/login/registration

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        validateAuth()
+    }
+    
+    @IBAction func logoutTapped(_ sender: Any) {
+        do {
+            try FirebaseAuth.Auth.auth().signOut()
+            
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+        catch {
+            print("Failed to log out")
+        }
+    }
+    
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
 
 }
 
--- Podfile
+++ Podfile
@@ -5,15 +5,9 @@ target 'Messenger' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for Messenger
+pod 'Firebase/Core'
+pod 'Firebase/Auth'
+pod 'Firebase/Database'
 
-  target 'MessengerTests' do
-    inherit! :search_paths
-    # Pods for testing
-  end
-
-  target 'MessengerUITests' do
-    # Pods for testing
-  end
 
 end
