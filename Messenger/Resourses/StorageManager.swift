8053feed96a7083fa1153b4e5a9f3d24b533b303
Evgeny <florans1807@mail.ru>
2023-12-12 16:23:20 +0300

    dewdwqdeqw

---
+++ Messenger/Resourses/StorageManager.swift
@@ -0,0 +1,8 @@
+//
+//  StorageManager.swift
+//  Messenger
+//
+//  Created by Флоранс on 22.11.2023.
+//
+
+import Foundation
---
+++ Messenger/Views/ConversationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ConversationTableViewCell.swift
+//  Messenger
+//
+//  Created by Флоранс on 02.12.2023.
+//
+
+import UIKit
+
+class ConversationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
